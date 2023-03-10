c84dfd67cf15be7e397453c75f8c16c42901f0ee
iwyfewwnt <tied10v3@mail.ru>
2023-03-10 15:17:28 +0300

    Update UwObject method implementations
    # Removed apache/commons-lang3 usage

--- src/main/java/io/github/u004/uwutils/UwObject.java
+++ src/main/java/io/github/u004/uwutils/UwObject.java
@@ -16,11 +16,8 @@
 
 package io.github.u004.uwutils;
 
-import io.vavr.*;
-import org.apache.commons.lang3.ObjectUtils;
-
-import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * An object utility.
@@ -35,379 +32,63 @@ import java.util.function.Function;
 public final class UwObject {
 
 	/**
-	 * Apply argument to the specified function if it's not null or return default value.
+	 * Apply argument to the specified function if it's not null or return a default value.
 	 *
 	 * @param object			argument to be applied
 	 * @param function			function to which apply argument
-	 * @param defaultValue		default value to be returned
+	 * @param defaultValue		default value to be returned on failure
 	 * @param <T>				argument type
 	 * @param <R>				return type
 	 * @return					result of the function applying or default value
 	 */
 	public static <T, R> R applyIfNotNull(T object, Function<T, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(object, function)) {
+		if (object == null || function == null) {
 			return defaultValue;
 		}
 
-		return ObjectUtils.defaultIfNull(function.apply(object), defaultValue);
+		R returnValue = function.apply(object);
+
+		if (returnValue == null) {
+			return defaultValue;
+		}
+
+		return returnValue;
 	}
 
 	/**
-	 * Apply argument to the specified function if it's not null or return null.
+	 * Apply argument to the specified function it's not null or return a default value.
 	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Function, Object)}.
+	 * @param object		argument to apply
+	 * @param function		function to which apply argument
+	 * @param supplier		supplier from which get the default value
+	 * @param <T>			argument type
+	 * @param <R>			return type
+	 * @return				result of the function applying or default value
+	 */
+	public static <T, R> R applyIfNotNull(T object, Function<T, R> function, Supplier<R> supplier) {
+		R returnValue = applyIfNotNull(object, function);
+
+		if (returnValue == null) {
+			return applyIfNotNull(supplier, Supplier::get);
+		}
+
+		return returnValue;
+	}
+
+	/**
+	 * Apply argument to the specified function if it's not null or return {@code null}.
+	 *
+	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Function, Object)}
+	 * w/ {@code null} as the default value.
 	 *
 	 * @param object			argument to be applied
 	 * @param function			function to which apply argument
 	 * @param <T>				argument type
 	 * @param <R>				return type
-	 * @return					result of the function applying or null
+	 * @return					result of the function applying or {@code null}
 	 */
 	public static <T, R> R applyIfNotNull(T object, Function<T, R> function) {
 		return applyIfNotNull(object, function, (R) null);
-	}
-
-	/**
-	 * Apply arguments to the specified function is they're not null or return default value.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param defaultValue		default value to be returned
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or default value
-	 */
-	public static <T1, T2, R> R applyIfNotNull(T1 obj1, T2 obj2, BiFunction<T1, T2, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(obj1, obj1, function)) {
-			return defaultValue;
-		}
-
-		return ObjectUtils.defaultIfNull(function.apply(obj1, obj2), null);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return null.
-	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Object, BiFunction, Object)}.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or null
-	 */
-	public static <T1, T2, R> R applyIfNotNull(T1 obj1, T2 obj2, BiFunction<T1, T2, R> function) {
-		return applyIfNotNull(obj1, obj2, function, (R) null);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return default value.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param defaultValue		default value to be returned
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or default value
-	 */
-	public static <T1, T2, T3, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, Function3<T1, T2, T3, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(obj1, obj2, obj3, function)) {
-			return defaultValue;
-		}
-
-		return ObjectUtils.defaultIfNull(function.apply(obj1, obj2, obj3), defaultValue);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return null.
-	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Object, Object, Function3, Object)}.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or null
-	 */
-	public static <T1, T2, T3, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, Function3<T1, T2, T3, R> function) {
-		return applyIfNotNull(obj1, obj2, obj3, function, (R) null);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return default value.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param defaultValue		default value to be returned
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or default value
-	 */
-	public static <T1, T2, T3, T4, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, Function4<T1, T2, T3, T4, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(obj1, obj2, obj3, obj4, function)) {
-			return defaultValue;
-		}
-
-		return ObjectUtils.defaultIfNull(function.apply(obj1, obj2, obj3, obj4), defaultValue);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or null.
-	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Object, Object, Object, Function4, Object)}.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or null
-	 */
-	public static <T1, T2, T3, T4, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, Function4<T1, T2, T3, T4, R> function) {
-		return applyIfNotNull(obj1, obj2, obj3, obj4, function, (R) null);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return default value.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param defaultValue		default value to be returned
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or default value
-	 */
-	public static <T1, T2, T3, T4, T5, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, Function5<T1, T2, T3, T4, T5, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(obj1, obj2, obj3, obj4, obj5, function)) {
-			return defaultValue;
-		}
-
-		return ObjectUtils.defaultIfNull(function.apply(obj1, obj2, obj3, obj4, obj5), defaultValue);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return null.
-	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Object, Object, Object, Object, Function5, Object)}.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or null
-	 */
-	public static <T1, T2, T3, T4, T5, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, Function5<T1, T2, T3, T4, T5, R> function) {
-		return applyIfNotNull(obj1, obj2, obj3, obj4, obj5, function, (R) null);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return default value.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param obj6 				6th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param defaultValue		default value to be returned
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <T6> 				6th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or default value
-	 */
-	public static <T1, T2, T3, T4, T5, T6, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, T6 obj6, Function6<T1, T2, T3, T4, T5, T6, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(obj1, obj2, obj3, obj4, obj5, obj6, function)) {
-			return defaultValue;
-		}
-
-		return ObjectUtils.defaultIfNull(function.apply(obj1, obj2, obj3, obj4, obj5, obj6), defaultValue);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return null.
-	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Object, Object, Object, Object, Object, Function6, Object)}.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param obj6 				6th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <T6> 				6th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or null
-	 */
-	public static <T1, T2, T3, T4, T5, T6, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, T6 obj6, Function6<T1, T2, T3, T4, T5, T6, R> function) {
-		return applyIfNotNull(obj1, obj2, obj3, obj4, obj5, obj6, function, (R) null);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return default value.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param obj6 				6th argument to be applied
-	 * @param obj7 				7th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param defaultValue		default value to be returned
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <T6> 				6th argument type
-	 * @param <T7> 				7th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or default value
-	 */
-	public static <T1, T2, T3, T4, T5, T6, T7, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, T6 obj6, T7 obj7, Function7<T1, T2, T3, T4, T5, T6, T7, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(obj1, obj2, obj3, obj4, obj5, obj6, obj7, function)) {
-			return defaultValue;
-		}
-
-		return ObjectUtils.defaultIfNull(function.apply(obj1, obj2, obj3, obj4, obj5, obj6, obj7), defaultValue);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return null.
-	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Object, Object, Object, Object, Object, Object, Function7, Object)}.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param obj6 				6th argument to be applied
-	 * @param obj7 				7th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <T6> 				6th argument type
-	 * @param <T7> 				7th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or null
-	 */
-	public static <T1, T2, T3, T4, T5, T6, T7, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, T6 obj6, T7 obj7, Function7<T1, T2, T3, T4, T5, T6, T7, R> function) {
-		return applyIfNotNull(obj1, obj2, obj3, obj4, obj5, obj6, obj7, function, (R) null);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return default value.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param obj6 				6th argument to be applied
-	 * @param obj7 				7th argument to be applied
-	 * @param obj8 				8th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param defaultValue		default value to be returned
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <T6> 				6th argument type
-	 * @param <T7> 				7th argument type
-	 * @param <T8> 				8th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or default value
-	 */
-	public static <T1, T2, T3, T4, T5, T6, T7, T8, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, T6 obj6, T7 obj7, T8 obj8, Function8<T1, T2, T3, T4, T5, T6, T7, T8, R> function, R defaultValue) {
-		if (ObjectUtils.anyNull(obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, function)) {
-			return defaultValue;
-		}
-
-		return ObjectUtils.defaultIfNull(function.apply(obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8), defaultValue);
-	}
-
-	/**
-	 * Apply arguments to the specified function if they're not null or return null.
-	 *
-	 * <p>Wraps {@link UwObject#applyIfNotNull(Object, Object, Object, Object, Object, Object, Object, Object, Function8, Object)}.
-	 *
-	 * @param obj1				1st argument to be applied
-	 * @param obj2				2nd argument to be applied
-	 * @param obj3 				3rd argument to be applied
-	 * @param obj4 				4th argument to be applied
-	 * @param obj5 				5th argument to be applied
-	 * @param obj6 				6th argument to be applied
-	 * @param obj7 				7th argument to be applied
-	 * @param obj8 				8th argument to be applied
-	 * @param function			function to which apply arguments
-	 * @param <T1>				1st argument type
-	 * @param <T2> 				2nd argument type
-	 * @param <T3> 				3rd argument type
-	 * @param <T4> 				4th argument type
-	 * @param <T5> 				5th argument type
-	 * @param <T6> 				6th argument type
-	 * @param <T7> 				7th argument type
-	 * @param <T8> 				8th argument type
-	 * @param <R>				return type
-	 * @return					result of the function applying or null
-	 */
-	public static <T1, T2, T3, T4, T5, T6, T7, T8, R> R applyIfNotNull(T1 obj1, T2 obj2, T3 obj3, T4 obj4, T5 obj5, T6 obj6, T7 obj7, T8 obj8, Function8<T1, T2, T3, T4, T5, T6, T7, T8, R> function) {
-		return applyIfNotNull(obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, function, null);
 	}
 
 	private UwObject() {
